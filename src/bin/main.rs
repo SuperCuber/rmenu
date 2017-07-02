a9c4eac201f373f27224596adeb677c71aad3583
SuperCuber <amit.gold00@gmail.com>
2017-07-02 12:35:55 +0300

    Move stuff around, also now its functioning. TODO: make i3 not shit itself when exiting

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,40 @@
 extern crate rmenu;
 
+use std::collections::HashMap;
+
 fn main() {
-    let ans = rmenu::iter(identity);
+    let mut options = HashMap::<String, String>::new();
+
+    // Add options
+
+    let mut converted_options = HashMap::new();
+    for entry in options {
+        converted_options.insert(entry.0.into(), entry.1.into());
+    }
+
+    let ans = rmenu::iter(|s| process(s, &converted_options));
     println!("Final: {:?}", ans);
 }
 
-fn identity(text: &str) -> String {
-    println!("Processing: {}", text);
-    String::from(text)
+fn filter(text: &str, options: &HashMap<String, String>) -> HashMap<String, String> {
+    let mut answer = options.clone();
+    answer.retain(|k, _| {
+        k.starts_with(text)
+    });
+    answer
 }
 
+fn process(text: &str, options: &HashMap<String, String>) -> String {
+    let answer = filter(text, options);
+    if answer.is_empty() {
+        return String::new();
+    }
+    let mut text_answer = String::new();
+    for (key, value) in answer {
+        text_answer.push_str(&key);
+        text_answer.push_str(" - ");
+        text_answer.push_str(&value);
+        text_answer.push('\n');
+    }
+    String::from(&text_answer[..text_answer.len() - 1])
+}
---
+++ src/gui.rs
@@ -0,0 +1,161 @@
+use ::std;
+
+use conrod::{self, widget, Colorable, Positionable, Widget};
+use conrod::backend::glium::glium;
+use conrod::backend::glium::glium::{DisplayBuild, Surface};
+
+use conrod::backend::glium::glium::glutin::VirtualKeyCode;
+
+struct State {
+    input_text: String,
+}
+
+pub fn iter<F>(process: F) -> Option<String>
+    where F: Fn(&str) -> String
+{
+    // Build the window.
+    let display = glium::glutin::WindowBuilder::new()
+        .with_vsync()
+        .with_fullscreen(glium::glutin::get_primary_monitor())
+        .with_title("Rmenu")
+        .with_multisampling(4)
+        .build_glium()
+        .unwrap();
+
+    // construct our `Ui`.
+    let dimensions = glium::glutin::get_primary_monitor().get_dimensions();
+    let mut ui = conrod::UiBuilder::new([dimensions.0 as f64, dimensions.1 as f64]).build();
+
+    // Generate the widget identifiers.
+    let ids = Ids::new(ui.widget_id_generator());
+
+    // Add a `Font` to the `Ui`'s `font::Map` from file.
+    const FONT_PATH: &'static str = "/usr/share/fonts/TTF/Ubuntu-M.ttf";
+    ui.fonts.insert_from_file(FONT_PATH).unwrap();
+
+    // A type used for converting `conrod::render::Primitives` into `Command`s that can be used
+    // for drawing to the glium `Surface`.
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+
+    // The image map describing each of our widget->image mappings (in our case, none).
+    let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+
+    let mut state = State { input_text: String::new() };
+
+    // Poll events from the window.
+    let mut last_update = std::time::Instant::now();
+    let mut ui_needs_update = true;
+    'main: loop {
+
+        // We don't want to loop any faster than 60 FPS, so wait until it has been at least
+        // 16ms since the last yield.
+        let sixteen_ms = std::time::Duration::from_millis(16);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        if duration_since_last_update < sixteen_ms {
+            std::thread::sleep(sixteen_ms - duration_since_last_update);
+        }
+
+        // Collect all pending events.
+        let mut events: Vec<_> = display.poll_events().collect();
+
+        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        if events.is_empty() && !ui_needs_update {
+            events.extend(display.wait_events().next());
+        }
+
+        // Reset the needs_update flag and time this update.
+        ui_needs_update = false;
+        last_update = std::time::Instant::now();
+
+        // Handle all events.
+        for event in events {
+
+            // Use the `winit` backend feature to convert the winit event to a conrod one.
+            if let Some(event) = conrod::backend::winit::convert(event.clone(), &display) {
+                ui.handle_event(event);
+                ui_needs_update = true;
+            }
+
+            match event {
+                // Break from the loop upon `Escape`.
+                glium::glutin::Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) |
+                    glium::glutin::Event::Closed => {
+                        break 'main;
+                    }
+                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Pressed,
+                                                    _,
+                                                    Some(keycode)) => {
+                    println!("Key: {:?}", keycode);
+                    match keycode {
+                        VirtualKeyCode::Up => {
+
+                        }
+                        VirtualKeyCode::Down => {
+
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        // Instantiate all widgets in the GUI.
+        {
+            let ui = &mut ui.set_widgets();
+            if let Some(answer) = set_widgets(ui, &ids, &mut state, &process) {
+                return Some(answer);
+            }
+        }
+
+        // Render the `Ui` and then display it on the screen.
+        if let Some(primitives) = ui.draw_if_changed() {
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0.0, 0.0, 0.0, 1.0);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
+        }
+    }
+
+    None
+}
+
+widget_ids!(struct Ids { canvas, scrollbar, input, output });
+
+fn set_widgets<F>(ui: &mut conrod::UiCell,
+                  ids: &Ids,
+                  state: &mut State,
+                  process: &F)
+                  -> Option<String>
+    where F: Fn(&str) -> String
+{
+    widget::Canvas::new()
+        .scroll_kids_vertically()
+        .color(conrod::color::DARK_CHARCOAL)
+        .set(ids.canvas, ui);
+
+    if let Some(edit) = widget::TextEdit::new(&state.input_text)
+            .color(conrod::color::WHITE)
+            .mid_top_of(ids.canvas)
+            .center_justify()
+            .set(ids.input, ui) {
+        state.input_text = edit;
+        // If contains \n, return this (without \n) as answer
+        if state.input_text.contains('\n') {
+            return Some(state.input_text.replace("\n", ""));
+        }
+    }
+
+    widget::Text::new(&process(&state.input_text))
+        .color(conrod::color::LIGHT_RED)
+        .middle_of(ids.canvas)
+        .center_justify()
+        .set(ids.output, ui);
+
+    widget::Scrollbar::y_axis(ids.canvas)
+        .auto_hide(true)
+        .set(ids.scrollbar, ui);
+
+    None
+}
