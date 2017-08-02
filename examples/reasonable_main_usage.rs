4b56ca7a0da596f56c65ec0d8684cfcbec49cfff
Amit Gold <amit.gold00@gmail.com>
2017-08-02 21:41:18 +0300

    Add proper example

---
+++ examples/reasonable_main_usage.rs
@@ -0,0 +1,21 @@
+extern crate rmenu;
+extern crate conrod;
+use conrod::color;
+
+fn main() {
+    let cmd = rmenu::Command::new;
+    rmenu::reasonable_main(
+        &vec![
+            cmd("11", "Display11", "notify-send Cmd11"),
+            cmd("12", "Display12", "notify-send Cmd12"),
+            cmd("2", "Display2", "notify-send Cmd2"),
+        ],
+        &rmenu::Config {
+            canvas_color: color::GRAY.with_alpha(0.1),
+            input_color: color::GRAY,
+            unselected_color: color::WHITE,
+            selected_color: color::BLACK,
+            ..Default::default()
+        },
+    ).unwrap();
+}
